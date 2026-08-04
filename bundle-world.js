d325401f133d3687356ad42f876f062f6c0e7f6f
Alejandra Cortés López <mimenu3d@gmail.com>
2026-08-04 13:55:23 +0200

    v2.06 (ar y bundle)
    Modificadas varias escalas y cosas, consultar Bitácora sobre 2.06.

--- bundle-world.js
+++ bundle-world.js
@@ -1,4 +1,4 @@
-﻿// 9th Wall v2.05
+﻿// 9th Wall v2.06
 (()=>{
   var e={
     574(){
@@ -111,7 +111,7 @@
             "shadowBias": 0,
             "shadowRadius": 2,
             "followCamera": false,
-            "shadowCamera": [-10, 10, 10, -10, 0.5, 200] // <-- AQUÍ ESTÁ LA SHADOW CAMERA
+            "shadowCamera": [-2, 2, 2, -2, 0.5, 200] // <-- AQUÍ ESTÁ LA SHADOW CAMERA. Valor por defecto: -10,10,10,-10.
           },
           "name": "Directional Light",
           "order": 0.6785011504707911
@@ -135,7 +135,8 @@
         // Cámara de Realidad Aumentada
         "52ba8a86-a459-4df8-b954-a570e85e0484": {
           "id": "52ba8a86-a459-4df8-b954-a570e85e0484",
-          "position": [0, 2, 3.211226375221431],
+          "position": [0, 0.2, 3.211226375221431], // Altura original, 2; cambiada a 0.2, lo que implica modificar escala de objeto en ar-world.html.
+
           "rotation": [-0.0004637899966810532, 0.9978406073902779, -0.06529682289718859, -0.007087458033270938],
           "scale": [1.0000000000000002, 1, 1.0000000000000004],
           "geometry": null,
@@ -211,7 +212,7 @@
           "id": "bc7753ae-2b39-4f48-910a-7921b756487b",
           "position": [0, 0, 0],
           "rotation": [-0.7071068, 0, 0, 0.7071068],
-          "scale": [20, 20, 20],
+          "scale": [10, 10, 10],
           "geometry": { "type": "plane", "width": 1, "height": 1 },
           "material": { "type": "shadow", "color": "#000000", "opacity": 0.4 },
           "parentId": "84028e73-ee70-412d-b8d4-c09bf07c655c",
@@ -234,7 +235,7 @@
           "id": "17af117a-efce-48dd-857e-e383a3649c7b",
           "position": [0, -0.01, 0],
           "rotation": [-0.707106799999999, 0, 0, 0.7071067623730954],
-          "scale": [20, 20, 20],
+          "scale": [10, 10, 10],
           "geometry": { "type": "plane", "width": 1, "height": 1 },
           "material": { "type": "hider" },
           "parentId": "84028e73-ee70-412d-b8d4-c09bf07c655c",
