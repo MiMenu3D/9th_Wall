a8fbe9b620ffa3f63b3e0fca32327e11634ff0a9
Alejandra Cortés López <mimenu3d@gmail.com>
2026-08-04 13:41:40 +0200

    v2.05 (Ar y Bundle)
    Escalas 50,50,50 bajadas a 20,20,20 y Light Estimation exagerada a ver qué hace.

--- bundle-world.js
+++ bundle-world.js
@@ -1,4 +1,4 @@
-﻿// 9th Wall v2.03
+﻿// 9th Wall v2.05
 (()=>{
   var e={
     574(){
@@ -211,7 +211,7 @@
           "id": "bc7753ae-2b39-4f48-910a-7921b756487b",
           "position": [0, 0, 0],
           "rotation": [-0.7071068, 0, 0, 0.7071068],
-          "scale": [50, 50, 50],
+          "scale": [20, 20, 20],
           "geometry": { "type": "plane", "width": 1, "height": 1 },
           "material": { "type": "shadow", "color": "#000000", "opacity": 0.4 },
           "parentId": "84028e73-ee70-412d-b8d4-c09bf07c655c",
@@ -234,7 +234,7 @@
           "id": "17af117a-efce-48dd-857e-e383a3649c7b",
           "position": [0, -0.01, 0],
           "rotation": [-0.707106799999999, 0, 0, 0.7071067623730954],
-          "scale": [50, 50, 50],
+          "scale": [20, 20, 20],
           "geometry": { "type": "plane", "width": 1, "height": 1 },
           "material": { "type": "hider" },
           "parentId": "84028e73-ee70-412d-b8d4-c09bf07c655c",
