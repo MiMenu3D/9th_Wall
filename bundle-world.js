e1a8bd14f87e9fdf2722fc39915df5dee12ec0c4
Alejandra Cortés López <mimenu3d@gmail.com>
2026-08-04 18:59:47 +0200

    v2.05 (regreso al checkpoint, cambios manuales)

--- bundle-world.js
+++ bundle-world.js
@@ -1,4 +1,4 @@
-﻿// 9th Wall v2.11
+﻿// 9th Wall v2.05
 (()=>{
   var e={
     574(){
@@ -40,7 +40,7 @@
           d=t.getEntity(r);
           d.setLocalPosition(i.data.worldPosition),
           d.set(e.Quaternion, e.math.quat.yRadians(Math.random()*Math.PI)),
-          t.events.dispatch(a, o, { worldPosition: i.data.worldPosition })
+          t.events.dispatch(a, o)
         })
       }
     });
@@ -99,7 +99,7 @@
         // Luz direccional y ShadowCamera
         "492cfe2c-9334-4a9c-a48a-be80132af9fb": {
           "id": "492cfe2c-9334-4a9c-a48a-be80132af9fb",
-          "position": [0.5, 3, 0.5], // Por defecto: 5, 25, 5
+          "position": [5, 25, 5],
           "rotation": [0, 0, 0, 1],
           "scale": [1, 1, 1],
           "geometry": null,
@@ -108,10 +108,10 @@
           "components": {},
           "light": {
             "type": "directional",
-            "shadowBias": 0, // Por defecto: 0
-            "shadowRadius": 5, // Por defecto: 2
+            "shadowBias": 0,
+            "shadowRadius": 2,
             "followCamera": false,
-            "shadowCamera": [-1, 1, 1, -1, 0.5, 50] // <-- AQUÍ ESTÁ LA SHADOW CAMERA. Valor por defecto: -10, 10, 10, -10, 0.5, 200.
+            "shadowCamera": [-10, 10, 10, -10, 0.5, 200] // <-- AQUÍ ESTÁ LA SHADOW CAMERA
           },
           "name": "Directional Light",
           "order": 0.6785011504707911
@@ -135,8 +135,7 @@
         // Cámara de Realidad Aumentada
         "52ba8a86-a459-4df8-b954-a570e85e0484": {
           "id": "52ba8a86-a459-4df8-b954-a570e85e0484",
-          "position": [0, 0.25, 3.211226375221431], // Altura original, 2; cambiada a 0.2, lo que implica modificar escala de objeto en ar-world.html.
-
+          "position": [0, 2, 3.211226375221431],
           "rotation": [-0.0004637899966810532, 0.9978406073902779, -0.06529682289718859, -0.007087458033270938],
           "scale": [1.0000000000000002, 1, 1.0000000000000004],
           "geometry": null,
@@ -212,7 +211,8 @@
           "id": "bc7753ae-2b39-4f48-910a-7921b756487b",
           "position": [0, 0, 0],
           "rotation": [-0.7071068, 0, 0, 0.7071068],
-          "scale": [20, 20, 20],
+          "scale": [10, 10, 10], // Por defecto: 50, 50, 50
+
           "geometry": { "type": "plane", "width": 1, "height": 1 },
           "material": { "type": "shadow", "color": "#000000", "opacity": 0.4 },
           "parentId": "84028e73-ee70-412d-b8d4-c09bf07c655c",
@@ -235,7 +235,7 @@
           "id": "17af117a-efce-48dd-857e-e383a3649c7b",
           "position": [0, -0.01, 0],
           "rotation": [-0.707106799999999, 0, 0, 0.7071067623730954],
-          "scale": [20, 20, 20],
+          "scale": [10, 10, 10], // Por defecto: 50, 50, 50
           "geometry": { "type": "plane", "width": 1, "height": 1 },
           "material": { "type": "hider" },
           "parentId": "84028e73-ee70-412d-b8d4-c09bf07c655c",
