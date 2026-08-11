3b824ce44dfcc0d9e5f6d176cf7c68f2570e68a6
Alejandra Cortés López <mimenu3d@gmail.com>
2026-08-11 22:16:46 +0200

    Update bundle-world.js

--- bundle-world.js
+++ bundle-world.js
@@ -1,4 +1,4 @@
-﻿// 9th Wall v2.65
+﻿// 9th Wall v2.66
 (()=>{
   var e={
     574(){
@@ -332,7 +332,7 @@
           "light": {
             "type": "directional",
             "shadowBias": 0.0001, // Previene artefactos de sombras a baja resolución (shadow acne)
-            "shadowRadius": 9,
+            "shadowRadius": 8,
             "followCamera": false,
             "shadowCamera": [-10, 10, 10,-10, 3, 5] // Versión anterior: 0.3, 0.3, 0.3, 0.3, 0.4, 1
           },
