cfd94eceda9c2a75ee0276c4a671bf3309f97451
Alejandra Cortés López <mimenu3d@gmail.com>
2026-08-11 14:55:46 +0200

    Update bundle-world.js

--- bundle-world.js
+++ bundle-world.js
@@ -1,4 +1,4 @@
-﻿// 9th Wall v2.57
+﻿// 9th Wall v2.59
 (()=>{
   var e={
     574(){
@@ -334,7 +334,7 @@
             "shadowBias": 0.0001, // Previene artefactos de sombras a baja resolución (shadow acne)
             "shadowRadius": 2,
             "followCamera": false,
-            "shadowCamera": [-0.3,0.3,0.3,-0.3,0.4,1] // Volumen óptimo para platos (1.5m)
+            "shadowCamera": [-10, 10, 10, -10, 0.5, 200] // Volumen óptimo para platos (1.5m)
           },
           "name": "Directional Light",
           "order": 0.6785011504707911
@@ -425,7 +425,7 @@
           "id": "bc7753ae-2b39-4f48-910a-7921b756487b",
           "position": [0, 0.001, 0],
           "rotation": [-0.7071068, 0, 0, 0.7071068],
-          "scale": [2, 2, 2],
+          "scale": [20, 20, 20],
           "geometry": { "type": "plane", "width": 1, "height": 1 },
           "material": { "type": "shadow", "color": "#000000", "opacity": 0.4 },
           "parentId": "84028e73-ee70-412d-b8d4-c09bf07c655c",
