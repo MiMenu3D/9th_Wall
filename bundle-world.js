33e04f04f01b12408f060c7285ffc90038b5fa63
Alejandra Cortés López <mimenu3d@gmail.com>
2026-08-11 14:12:09 +0200

    Update version to 2.56 and modify shadowCamera

--- bundle-world.js
+++ bundle-world.js
@@ -1,4 +1,4 @@
-﻿// 9th Wall v2.55
+﻿// 9th Wall v2.56
 (()=>{
   var e={
     574(){
@@ -334,7 +334,7 @@
             "shadowBias": 0.0001, // Previene artefactos de sombras a baja resolución (shadow acne)
             "shadowRadius": 2,
             "followCamera": false,
-            "shadowCamera": [-1,1,1,-1,0.5,2] // Volumen óptimo para platos (1.5m)
+            "shadowCamera": [-0.5,0.5,0.5,-0.5,0.5,1] // Volumen óptimo para platos (1.5m)
           },
           "name": "Directional Light",
           "order": 0.6785011504707911
