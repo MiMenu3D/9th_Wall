ef06e06bba5274486cac2cdf1f80bc75e1d1ddfb
Alejandra Cortés López <mimenu3d@gmail.com>
2026-08-15 14:55:24 +0200

    v3.02 Apple Probe (AR y Bundle)

--- bundle-world.js
+++ bundle-world.js
@@ -1,4 +1,4 @@
-﻿// 9th Wall v3.01 Apple Probe
+﻿// 9th Wall v3.02 Apple Probe
 (() => {
   var e = {
     574() {
@@ -341,7 +341,7 @@
           "prefab": true
         },
 
-        // Luz direccional y ShadowCamera (Restaurada la estructura original del bundle)
+        // Luz direccional 1: Contact AO y sombras base (Intensidad 0.00 en runtime)
         "492cfe2c-9334-4a9c-a48a-be80132af9fb": {
           "id": "492cfe2c-9334-4a9c-a48a-be80132af9fb",
           "position": [0, 1, 0],
@@ -360,6 +360,28 @@
           },
           "name": "Directional Light",
           "order": 0.6785011504707911
+        },
+
+        // Luz direccional 2: Luz física vinculada al Probe (Especular directo y sombra secundaria)
+        "f642cfe2-9334-4a9c-a48a-be80132af9fc": {
+          "id": "f642cfe2-9334-4a9c-a48a-be80132af9fc",
+          "position": [0, 1.8, 0], // Inicia cenital a < 2m del origen
+          "rotation": [0, 0, 0, 1],
+          "scale": [1, 1, 1],
+          "geometry": null,
+          "material": null,
+          "parentId": "84028e73-ee70-412d-b8d4-c09bf07c655c",
+          "components": {},
+          "light": {
+            "type": "directional",
+            "shadowBias": 0.0001,
+            "shadowRadius": 2, // Sombra más definida (radio 2). Bajar a 1 en pruebas si se desea más nítida
+            "shadowIntensity": 0.5, // 50% de intensidad relativa respecto a la sombra del Ground
+            "followCamera": false,
+            "shadowCamera": [-2, 2, 2, -2, 0.1, 4] // Frustum optimizado para distancias <= 1.8m
+          },
+          "name": "Directional Light 2",
+          "order": 0.679
         },
 
         // Camera de Realidad Aumentada
